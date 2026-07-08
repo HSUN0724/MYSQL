379fdc810579864dff785a768aaeb4e6df4bb584
unknown <alictw@yahoo.com.tw>
2026-07-08 19:22:09 +0800

    20260708

--- SQL File22.sql
+++ SQL File22.sql
@@ -34,3 +34,43 @@ IGNORE 1 ROWS
 SET age = NULLIF(@age,''), fare = nullif(@fare, ''), body = nullif(@body, '');
 
 select * from full_passengers limit 1350;
+
+#1
+select * from full_passengers limit 1350;
+select sex, count(sex) as gender_counts from full_passengers group by sex;
+
+#2
+select * from full_passengers limit 1350;
+select id ,pname from full_passengers limit 590 , 293;
+
+#3
+select * from full_passengers limit 1350;
+select pname, survived from full_passengers where pname like 'Andersson,%' and ticket = 347082;
+
+#4
+select * from full_passengers limit 1350;
+select * from full_passengers where pname like '%Brogren%';
+select * from full_passengers where ticket = 347080;
+
+#5
+select * from full_passengers limit 1350;
+select * from full_passengers where pname like '%Leonard%' and sex = 'male';
+select * from full_passengers where pname like '%Leonard%' and sex = 'male' and ticket not in ('LINE');
+select  id , pclass , pname from full_passengers where pname like '%Leonard%' and sex = 'male' and ticket not in ('LINE');
+
+#6
+select * from full_passengers limit 1350;
+select  ticket, count(ticket) as ticket_count from full_passengers group by ticket order by count(ticket) desc limit 1;
+
+#7
+select * from full_passengers limit 1350;
+# select pclass, sex, pname , avg(age) from full_passengers where sex = 'male' and pclass != '1' group by sex;
+select pclass, sex, avg(age) from full_passengers where sex = 'male' and pclass != '1' group by pclass;
+
+#8
+select * from full_passengers limit 1350;
+select embarked as '登船點', count(embarked) as '登船人數', round(count(embarked)/(select count(*) from full_passengers)*100,2)as '登船點占百分比' from full_passengers where embarked != '' group by embarked;
+select count(*) from full_passengers;
+
+
+
